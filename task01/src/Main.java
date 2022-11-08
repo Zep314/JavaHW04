397910ce8bc3caaa784d4e2e1fee7116c66f616e
Dmitry Mordanov <mordanov314@gmail.com>
2022-11-08 16:34:14 +0300

    commit1

--- task01/src/Main.java
+++ task01/src/Main.java
@@ -2,9 +2,22 @@
 Задание 1.
 Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
  */
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
+        try {
+            LogManager.getLogManager().readConfiguration( // берем конфиг для логов
+                    Main.class.getResourceAsStream("./log.config"));
+        } catch (IOException e) {  // печаль, беда...
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+        //log.setLevel(Level.FINE);
+        log.info("Начинаем работу, инициализируем массив случайной величины");
 
         System.out.println("Hello world!");
+        log.info("Работа завершена");
     }
 }
